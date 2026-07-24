6d743b357d7f861c2e63de02a862fd902c43133b
muhammadanas8454-art <muhammad.anas8454@gmail.com>
2026-07-24 23:36:32 +0400

    Update server.js
    test6

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ const DASHBOARD_PASSWORD = 'restaurant123';
 // Profile inventory
 const venueProfiles = {
     "lisbon_brunch": {
-        name: "Lisbon Brunch Co.",
+        name: "Sharjah Brunch Co.",
         color: "#d4a373",
         logo: "https://images.unsplash.com/photo-1554118811-1e0d58224f24?w=100"
     },
     "Lisbon_brunch": {
-        name: "Lisbon Brunch Co.",
+        name: "Sharjah Brunch Co.",
         color: "#d4a373",
         logo: "https://images.unsplash.com/photo-1554118811-1e0d58224f24?w=100"
     },
